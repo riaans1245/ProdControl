5b27754a7e0001d85e91d5ff7db2721c7accc938
Riaan Strydom <riaan.strydom@maszengrange.co.nz>
2026-09-25 08:31:15 +1200

    25/9/26-1

--- test1233/wwwroot/js/site.js
+++ test1233/wwwroot/js/site.js
@@ -1,14 +1,38 @@
+   
     $(document).ready(function () {
-    // 1. Automatically show the loader whenever ANY form in the app is submitted
-    $('form').on('submit', function () {
-        // If the form inputs are valid, show the full-screen spinner overlay
+    
+    // Intercept form submissions
+    $('form').on('submit', function (e) {
+        
+        // Only run if the form passes validation checks
         if ($(this).valid()) {
-            $('#global-loader').removeClass('d-none');
-        }
-    });
+            
+            // 1. Prevent the form from submitting instantly
+            e.preventDefault();
+            var currentForm = this;
 
-    // 2. Automatically show the loader if they click direct links that might take time
-    $('.show-loader-link').on('click', function () {
-        $('#global-loader').removeClass('d-none');
+            // 2. Un-hide the full-screen spinner overlay
+            $('#global-loader').removeClass('d-none');
+
+            // 3. Track two independent conditions: 
+            //    A) Has 2 seconds passed? 
+            //    B) Is the browser ready to submit?
+            var timerFinished = false;
+
+            // Start the 2-second countdown clock
+            setTimeout(function () {
+                timerFinished = true;
+                submitWhenReady();
+            }, 2000); // 2000 milliseconds = 2 seconds
+
+            function submitWhenReady() {
+                if (timerFinished) {
+                    // 4. Finally release the form to submit to the database.
+                    // If the server takes *longer* than 2 seconds to load, 
+                    // the spinner will naturally stay on screen until the page changes.
+                    currentForm.submit();
+                }
+            }
+        }
     });
 });
